f8ecc17a6e2715029d9aa0a8c030c9303010c8c3
vicharatb <tharkana98@gmail.com>
2025-08-29 14:51:46 +0530

    Add parameters and  GUI globals

--- KeyboardIllumination.py
+++ KeyboardIllumination.py
@@ -1,1 +1,22 @@
 "# KeyboardIllumination" 
+import numpy as np
+import sounddevice as sd
+from pynput import keyboard
+from pynput.keyboard import Controller, Key
+import threading
+import time
+import tkinter as tk
+
+keyboard_controller = Controller()
+
+# Parameters
+fs = 100000
+block_duration = 0.02
+threshold = 0.05
+last_bass_state = False
+running = True
+current_bass = 0
+
+# GUI globals
+root = None
+bass_label = None
